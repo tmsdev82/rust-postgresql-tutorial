26ed532ea08c8a61ee202c37b15dda70e4280c77
tim <tmsteem82@gmail.com>
2021-10-29 15:15:15 +0200

    added queries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use postgres::{Client, Error, NoTls};
+
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect(
+        "postgresql://dboperator:operatorpass123@localhost:5243/postgres",
+        NoTls,
+    )?;
+
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS app_user (
+            id              SERIAL PRIMARY KEY,
+            username        VARCHAR UNIQUE NOT NULL,
+            password        VARCHAR NOT NULL,
+            email           VARCHAR UNIQUE NOT NULL
+            )
+    ",
+    )?;
+
+    client.execute(
+        "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
+        &[&"user1", &"mypass", &"user@test.com"],
+    )?;
+
+    Ok(())
 }
